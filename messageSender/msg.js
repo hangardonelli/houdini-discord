ffe0de1ea9c621d65ae4aa7fb0ca4859aa1c5e48
Ian Black <166574067+iannblack@users.noreply.github.com>
2024-11-13 11:04:43 -0500

    Update msg.js

--- messageSender/msg.js
+++ messageSender/msg.js
@@ -1,24 +1,15 @@
 const db = require('../db/database.js');
 const { MessageEmbed } = require('discord.js');
-const configuration = require('../config.json')
+const { color, name } = require('../config.json');
 
+async function embed(channel, message, title = null) {
+  const embed = new MessageEmbed()
+    .setColor(color)
+    .setTitle(title ? `${title} - ${name}` : name)
+    .setDescription(message)
+    .setTimestamp();
 
-async function embed(channel, message, title = null){
-   
-
-        
-        const embed = new MessageEmbed()
-        .setColor(configuration.color)
-        .setTitle(title == null ? configuration.name : `${title} - ${configuration.name}`)
-        .setDescription(message)
-      
-        .setTimestamp()
-    
-    channel.send({ embeds: [embed] });
-    
- 
+  channel.send({ embeds: [embed] });
 }
 
 module.exports = { embed };
-
-
